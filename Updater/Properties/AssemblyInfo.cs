2d0159f71df20e141674daaeb3c51ad2d04e882e
rex706 <apsinakis@me.com>
2021-01-20 16:41:31 -0500

    1.1.0.3 Hotfix

--- Updater/Properties/AssemblyInfo.cs
+++ Updater/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Updater")]
+[assembly: AssemblyTitle("Simple file updater to be used for other applications.")]
 [assembly: AssemblyDescription("Simple file updater to be used for other applications.")]
 [assembly: AssemblyConfiguration("release")]
 [assembly: AssemblyCompany("rex706 Industries")]
