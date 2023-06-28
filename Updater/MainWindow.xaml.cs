b955522ecc79bf1bd39025097a4b4668256da285
rex706 <apsinakis@me.com>
2023-06-27 20:00:13 -0400

    Execute update in separate thread
    * Use a task to run the update process
    * Disable TopMost and just Focus the main window on launch
    * Wait for in-use files to be closed and prompt user

--- Updater/MainWindow.xaml.cs
+++ Updater/MainWindow.xaml.cs
@@ -5,8 +5,10 @@ using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace Updater
 {
@@ -26,6 +28,8 @@ namespace Updater
     {
         private WebClient webClient;               // Our WebClient that will be doing the downloading for us.
         private Stopwatch sw = new Stopwatch();    // The stopwatch which we will be using to calculate the download speed.
+        private readonly int sleep = 1000;
+        private readonly int maxSleep = 10000;
 
         public MainWindow()
         {
@@ -44,7 +48,9 @@ namespace Updater
 
             string manifestUrl = null;
 
-            foreach(string arg in args)
+            Focus();
+
+            foreach (string arg in args)
             {
                 if (Uri.TryCreate(arg, UriKind.Absolute, out _) && !File.Exists(arg))
                 {
@@ -60,78 +66,95 @@ namespace Updater
                 return;
             }
 
-            ConsoleBox.AppendText("Reading update manifest . . .\n");
+            ConsoleBox.AppendText("Reading update manifest . . .\n" + manifestUrl + "\n");
 
             List<Download> downloads = new List<Download>();
 
-            // Open the text file using a stream reader.
-            using (WebClient client = new WebClient())
+            try
             {
                 // Open the text file using a stream reader.
-                using (Stream stream = await client.OpenReadTaskAsync(manifestUrl))
+                using (WebClient client = new WebClient())
                 {
-                    StreamReader reader = new StreamReader(stream);
-
-                    // Initialize variables.
-                    Version latest = null;
-                    string executable = null;
-
-                    try
-                    {
-                        // First two expected items in manifest will be the new version and the starting executable.
-                        string v = await reader.ReadLineAsync();
-                        latest = Version.Parse(v);
-                        executable = await reader.ReadLineAsync();
-                    }
-                    catch (Exception m)
-                    {
-                        MessageBox.Show("Error parsing new version number or launch executable.\n\n" + m.Message + "\n\nManifest: " + manifestUrl);
-                        Close();
-                        return;
-                    }
-
-                    // Load manifest urls and file names.
-                    while (!reader.EndOfStream)
+                    // Open the text file using a stream reader.
+                    using (Stream stream = await client.OpenReadTaskAsync(manifestUrl))
                     {
                         try
                         {
-                            // Expected in pairs of two.
-                            string downloadUrl = await reader.ReadLineAsync();
-                            string downloadFile = await reader.ReadLineAsync();
+                            StreamReader reader = new StreamReader(stream);
 
-                            if (downloadUrl == null || downloadFile == null)
+                            // Initialize variables.
+                            Version latest = null;
+                            string executable = null;
+
+                            try
                             {
-                                MessageBox.Show("Uneven update manifest.\nPlease review format.");
+                                // First two expected items in manifest will be the new version and the starting executable.
+                                string v = await reader.ReadLineAsync();
+                                latest = Version.Parse(v);
+                                executable = await reader.ReadLineAsync();
+                            }
+                            catch (Exception m)
+                            {
+                                MessageBox.Show("Error parsing new version number or launch executable.\n\n" + m.Message + "\n\nManifest: " + manifestUrl);
                                 Close();
                                 return;
                             }
 
-                            // If a new version of the updater is being acquired, rename it to be handled in UpdateCheck.cs
-                            if (downloadFile == "Updater.exe")
+                            // Load manifest urls and file names.
+                            while (!reader.EndOfStream)
                             {
-                                downloads.Add(new Download { url = downloadUrl, file = "Updater_new.exe" });
-                            }
-                            else
-                            {
-                                downloads.Add(new Download { url = downloadUrl, file = downloadFile });
-                            }
-                        }
-                        catch (Exception m)
-                        {
-                            MessageBox.Show("Uneven update manifest.\nPlease review format.");
-                            Close();
-                            return;
-                        }
-                    }
-                    if (downloads.Count == 0)
-                    {
-                        MessageBox.Show("No download arguments found in manifest.");
-                        Close();
-                        return;
-                    }
+                                try
+                                {
+                                    // Expected in pairs of two.
+                                    string downloadUrl = await reader.ReadLineAsync();
+                                    string downloadFile = await reader.ReadLineAsync();
 
-                    Update(executable, downloads);
+                                    if (downloadUrl == null || downloadFile == null)
+                                    {
+                                        MessageBox.Show("Uneven update manifest.\nPlease review format.");
+                                        Close();
+                                        return;
+                                    }
+
+                                    // If a new version of the updater is being acquired, rename it to be handled in UpdateCheck.cs
+                                    if (downloadFile == "Updater.exe")
+                                    {
+                                        downloads.Add(new Download { url = downloadUrl, file = "Updater_new.exe" });
+                                    }
+                                    else
+                                    {
+                                        downloads.Add(new Download { url = downloadUrl, file = downloadFile });
+                                    }
+                                }
+                                catch (Exception m)
+                                {
+                                    MessageBox.Show("Uneven update manifest.\nPlease review format.");
+                                    Close();
+                                    return;
+                                }
+                            }
+                            if (downloads.Count == 0)
+                            {
+                                MessageBox.Show("No download arguments found in manifest.");
+                                Close();
+                                return;
+                            }
+
+                            ConsoleBox.AppendText("Starting update sequence . . .\n");
+                            Task.Run(() => Update(executable, downloads));
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error initilaizing stream reader for update manifest\n\n" + ex.Message);
+                            Close();
+                        }
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error reading the update manifest\n\n" + manifestUrl + "\n\n" + ex.Message);
+                Close();
             }
         }
 
@@ -143,26 +166,33 @@ namespace Updater
                 await DownloadFile(download.url, download.file);
             }
 
-            ConsoleBox.AppendText("Update complete!\n");
+            Dispatcher.Invoke(() => { ConsoleBox.AppendText("Update complete!\n"); });
             //MessageBox.Show("Update complete!");
 
             // First argument is the program to be opened when the update is complete.
             if (executable != null && executable.Length > 1 && executable.Contains(".exe"))
             {
-                ConsoleBox.AppendText("Attempting to start" + executable + " . . .\n");
+                Dispatcher.Invoke(() => { ConsoleBox.AppendText("Attempting to start '" + executable + "' . . .\n"); });
+
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.UseShellExecute = true;
+                info.FileName = executable;
 
                 try
                 {
-                    Process.Start(executable);
+                    Process.Start(info);
                 }
                 catch (Exception e)
                 {
                     MessageBox.Show(e.Message);
                 }
             }
-            
-            // Close updater.
-            Close();
+
+            Dispatcher.Invoke(() => { ConsoleBox.AppendText("Closing updater . . . \n"); });
+
+            Thread.Sleep(sleep);
+
+            Dispatcher.Invoke(() => { Close(); });
         }
 
         public async Task DownloadFile(string urlAddress, string fileName)
@@ -174,21 +204,41 @@ namespace Updater
 
                 string path = AppDomain.CurrentDomain.BaseDirectory + fileName;
 
-                // Delete files to update.
-                if (File.Exists(path))
-                {
-                    File.Delete(path);
-                }
-
-                // The variable that will be holding the url address (making sure it starts with http://).
-                Uri URL = new Uri(urlAddress);
-
-                // Start the stopwatch which will be used to calculate the download speed.
-                sw.Start();
-                ConsoleBox.AppendText("Downloading '" + fileName + "' . . .\n");
-
                 try
                 {
+                    // Delete files to update.
+                    if (File.Exists(path))
+                    {
+                        int totalSleep = 0;
+
+                        while (IsFileLocked(path))
+                        {
+                            if (totalSleep > maxSleep)
+                            {
+                                MessageBox.Show("Please close " + fileName + " and click OK");
+                            }
+
+                            Thread.Sleep(sleep);
+
+                            Dispatcher.Invoke(() => { ConsoleBox.AppendText("Waiting for " + fileName + " to close . . .\n"); });
+
+                            totalSleep += sleep;
+                        }
+
+                        File.Delete(path);
+                    }
+
+                    // The variable that will be holding the url address (making sure it starts with http://).
+                    Uri URL = new Uri(urlAddress);
+
+                    // Start the stopwatch which will be used to calculate the download speed.
+                    sw.Start();
+
+                    Dispatcher.Invoke(() =>
+                    {
+                        ConsoleBox.AppendText("Downloading '" + fileName + "' . . .\n");
+                    });
+
                     // Start downloading the file.
                     await webClient.DownloadFileTaskAsync(URL, path);
                 }
@@ -230,9 +280,35 @@ namespace Updater
 
             if (e.Cancelled == true)
             {
-                ConsoleBox.AppendText("Download has been cancelled.\n");
-                MessageBox.Show("Download has been canceled.");
+                Dispatcher.Invoke(() => { ConsoleBox.AppendText("Download has been cancelled.\n"); });
             }
+        }
+
+        protected virtual bool IsFileLocked(string filePath)
+        {
+            try
+            {
+                using (FileStream stream = new FileStream(filePath, FileMode.Open))
+                {
+                    stream.Close();
+                }
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+
+            //file is not locked
+            return false;
+        }
+
+        private void ConsoleBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ConsoleBox.ScrollToEnd();
         }
     }
 }
